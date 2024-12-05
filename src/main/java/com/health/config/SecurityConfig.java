e5c35ce0e8c23f2133122ad88101b7610c7d8abe
Alok Kumar <59380269+AlokSP@users.noreply.github.com>
2024-12-05 12:09:01 +0530

    Package Name Edit

--- src/main/java/com/health/config/SecurityConfig.java
+++ src/main/java/com/health/config/SecurityConfig.java
@@ -90,7 +90,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             "/addPreRequisticWhenNotRequired/**", "/addPreRequistic/**", "/addVideo/**", "/addSlide/**",
             "/addScript/**", "/listTutorialForContributorReview/**", "/Contributor/review/**", "/uploadTimescript/**",
             "/addLiveTutorial", "/updateLiveTutorial", "/addSpokenVideo", "/spokenVideo/edit/**", "/addVideoResource",
-            "/createPackage",
+            "/createPackage", "/packageName/edit/**", "/updatePackageName",
 
     };
 
