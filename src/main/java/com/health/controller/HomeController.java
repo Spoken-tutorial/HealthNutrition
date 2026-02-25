b7586679ebe8513b71829e4748432017ba26903b
Alok Kumar <59380269+AlokSP@users.noreply.github.com>
2026-02-25 10:14:39 +0530

    all districts

--- src/main/java/com/health/controller/HomeController.java
+++ src/main/java/com/health/controller/HomeController.java
@@ -6671,6 +6671,10 @@ public class HomeController {
         states.sort(Comparator.comparing(State::getStateName));
         model.addAttribute("states", states);
 
+        Optional<District> allDis = districtService.findByDistrictNameIgnoreCase(CommonData.ALL_DISTRICTS);
+
+        allDis.ifPresent(dis -> model.addAttribute("allDisId", dis.getId()));
+
         List<ProjectReport> projectReportList = new ArrayList<>();
         List<ProjectReport> tempProjectReportList = projectReportService.findAll();
         for (ProjectReport temp : tempProjectReportList) {
